b3f9effd7f13b977ce45ea8232231286a3506974
Tiago Malveira <tmalveirac@gmail.com>
2018-02-11 21:03:38 -0300

    Python - Listas, Dicts, Set

---
+++ exemplos/my_math.py
@@ -0,0 +1,5 @@
+def area_quadrado(lado):
+	return lado * lado
+
+def area_ret(base, altura):
+	return base * altura
---
+++ exemplos/teste.py
@@ -0,0 +1,8 @@
+#import my_math as x
+
+#print(x.area_quadrado(6))
+
+
+from my_math import area_quadrado
+
+print(area_quadrado(4)) 
